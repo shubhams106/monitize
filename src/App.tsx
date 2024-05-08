d693b606921d3acf66176fb752599df62036af86
manish saraan <manish021hs@gmail.com>
2024-05-08 18:03:48 +0530

    final css changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import "./App.css";
 import * as React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { ThemeProvider } from "./components/context/ThemeProvider";
 import ImagesSection from "./components/imagesSection/ImagesSection";
 
-function App({ children }: { children: React.ReactNode }) {
+function App() {
   return (
     <>
       <ThemeProvider>
-        <main className="background-light850_dark100 relative w-full h-full bg-blue-300">
+        <main className="background-light850_dark100 relative w-full h-full">
           <Navbar />
 
           <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14 ">
