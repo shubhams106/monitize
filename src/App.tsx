3393e96d75f423c31a5a515d8b3564ba80fe65be
shubham <singlas106@gmail.com>
2024-05-08 20:51:11 +0530

    changes in ui of imgs section

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,14 @@ import { ThemeProvider } from "./components/context/ThemeProvider";
 import ImagesSection from "./components/imagesSection/ImagesSection";
 
 function App() {
-  const [data, setData] = React.useState([]);
-  const [Finaldata, setFinalData] = React.useState([]);
-
-  const fetchData: any = () => {
-    fetch("https://www.reddit.com/r/MostBeautiful.json?limit=8&after=1cmihmg")
-      .then((response) => response.json())
-      .then((d) => setData(d.data.children));
-  };
-
-  React.useEffect(() => {
-    fetchData();
-  }, []);
-
-  React.useEffect(() => {
-    const tempArr = [];
-    for (let i = 0; i < data.length; i++) {
-      const obj = data[i].data;
-      tempArr.push({
-        title: obj.title,
-        url: obj.url,
-        id: obj.id,
-      });
-    }
-    setFinalData(tempArr);
-  }, [data]);
-  console.log({ Finaldata });
-
   return (
     <>
       <ThemeProvider>
         <main className="background-light850_dark100 relative w-full h-full">
           <Navbar />
 
-          <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14 ">
-            <div className="mx-auto w-full max-w-5xl text-primary-500">
+          <section className="flex min-h-screen flex-1 flex-col px-3 pb-6 pt-36 max-md:pb-14 sm:px-14 ">
+            <div className="mx-auto w-full max-w-8xl text-primary-500">
               <ImagesSection />
             </div>
           </section>
--- src/components/imagesSection/ImagesSection.tsx
+++ src/components/imagesSection/ImagesSection.tsx
@@ -1,7 +1,68 @@
-import React from "react";
-
+import { title } from "process";
+import React, { useEffect, useState } from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 const ImagesSection = () => {
-  return <div className="h1-bold">ImagesSection</div>;
+  const [data, setData] = useState([]);
+  const [Finaldata, setFinalData] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://www.reddit.com/r/MostBeautiful.json?limit=8&after=1cmihmg")
+      .then((response) => response.json())
+      .then((d) => setData(d.data.children));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const tempArr = [];
+    for (let i = 0; i < data.length; i++) {
+      const obj = data[i].data;
+      tempArr.push({
+        title: obj.title,
+        url: obj.url,
+        id: obj.id,
+      });
+    }
+    setFinalData(tempArr);
+  }, [data]);
+  return (
+    <div className="">
+      <div className="h1-bold mb-32 text-center">ImagesSection</div>
+      <div className="flex gap-8 overflow-hidden flex-wrap">
+        {Finaldata &&
+          Finaldata.length > 0 &&
+          Finaldata.map((item) => (
+            <div className="flex flex-col gap-4 max-w-[250px]">
+              <img src={item.url} alt="url" className="h-[300px] w-[250px]" />
+              <p className="line-clamp-2 mb-3">{item?.title}</p>
+            </div>
+          ))}
+      </div>
+      <div className="">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
+    </div>
+  );
 };
 
 export default ImagesSection;
